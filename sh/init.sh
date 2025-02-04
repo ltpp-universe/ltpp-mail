ca25405ec35d1715f2d43ee4f65f6453487b5b84
尤雨东 <root@ltpp.vip>
2025-02-04 09:09:46 +0800

    feat: sh

--- sh/init.sh
+++ sh/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git config core.ignorecase false;
-git remote add ltpp ssh://git@git.ltpp.vip:66/root/ltpp-mail.git;
+git remote add jihulab ssh://jihulab.com:ltpp/ltpp-mail.git;
 git remote add github git@github.com:ltpp-universe/ltpp-mail.git;
 git remote add origin git@github.com:ltpp-universe/ltpp-mail.git;
